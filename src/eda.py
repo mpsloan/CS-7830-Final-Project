7baf5e3661221270af5b6e97b539d99bfbc40fa7
mpsloan <sloan.40@wright.edu>
2026-04-26 17:26:06 -0400

    revert feature correlation parameter

--- src/eda.py
+++ src/eda.py
@@ -2,17 +2,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
-def visualize_feature_correlation(df: pd.DataFrame, plot_path: str) -> None:
+def visualize_feature_correlation(df: pd.DataFrame, combine_features: list, plot_path: str) -> None:
     """
     Visualizes the correlation matrix of the numeric combine features.
 
     Args:
         df (pd.DataFrame): The preprocessed input DataFrame.
+        combine_features (list): The labels for each class
         plot_path (str): The file path to save the plot.
     """
-    # Only plot the core numeric combine features
-    combine_features = ['Ht', 'Wt', 'Forty', 'Vertical', 'BenchReps', 'BroadJump', 'Cone', 'Shuttle', 'Draft_Position']
-
     # This computes the Pearson correlation (r) for all numeric columns
     corr_matrix = df[combine_features].corr()
 
@@ -58,5 +56,7 @@ def run_eda(df: pd.DataFrame) -> None:
     Args:
         df (pd.DataFrame): The preprocessed DataFrame.
     """
-    visualize_feature_correlation(df, "plots/combine_data_correlation.png")
+    # List of combine features that we want (excludes position from feature list)
+    combine_features = ['Ht', 'Wt', 'Forty', 'Vertical', 'BenchReps', 'BroadJump', 'Cone', 'Shuttle', 'Draft_Position']
+    visualize_feature_correlation(df, combine_features, "plots/combine_data_correlation.png")
     visualize_class_distribution(df, 'Draft_Position', "plots/combine_data_distribution.png")
